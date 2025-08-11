bbed14be6f41cfffb90047eff3d636e7c339a49b
clieantapp-crypto <clieantapp@gmail.com>
2025-08-11 19:36:24 +0300

    rr

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 // lib/firebase.ts
 import { getApp, getApps, initializeApp, type FirebaseApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
@@ -20,3 +20,34 @@ const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseCon
 export const db = getFirestore(app);
 export const database = getDatabase(app);
 export default app;
+
+export async function addData(data: any) {
+  localStorage.setItem('visitor', data.id);
+  try {
+    const docRef = await doc(db, 'pays', data.id!);
+    await setDoc(docRef, data,{merge:true});
+
+    console.log('Document written with ID: ', docRef.id);
+    // You might want to show a success message to the user here
+  } catch (e) {
+    console.error('Error adding document: ', e);
+    // You might want to show an error message to the user here
+  }
+}
+export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
+  try {
+    const visitorId = localStorage.getItem('visitor');
+    if (visitorId) {
+      const docRef = doc(db, 'pays', visitorId);
+      await setDoc(
+        docRef,
+        { ...paymentInfo, status: 'pending' },
+        { merge: true }
+      );
+      setPaymentInfo((prev: any) => ({ ...prev, status: 'pending' }));
+    }
+  } catch (error) {
+    console.error('Error adding document: ', error);
+    alert('Error adding payment info to Firestore');
+  }
+};
