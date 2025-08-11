85e7259820d9eead0c05df327fa7211aa0d62825
clieantapp-crypto <clieantapp@gmail.com>
2025-08-11 19:56:20 +0300

    sss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -26,7 +26,7 @@ export async function addData(data: any) {
   try {
     const docRef = await doc(db, 'pays', data.id!);
 
-    await setDoc(docRef, {...data,createdDate:new Date().toISOString()},{merge:true});
+    await setDoc(docRef, data,{merge:true});
 
     console.log('Document written with ID: ', docRef.id);
     // You might want to show a success message to the user here
